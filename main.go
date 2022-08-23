5115732ca3da6ab88ebad1a5e945d1eb7a8f5069
dimmWork <witty.sock3113@fastmail.com>
2022-08-23 19:21:11 +0200

    2

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
+// https://leetcode.com/problems/reverse-integer/
+
 package main
 
 import "fmt"
 
 func main() {
 
-	fmt.Println(reverse(46246846))
+	fmt.Println(reverse(123456789))
 
 }
 
 func reverse(x int) int {
-
+	r := 0
+	for i := 1; i < x; i = i * 10 {
+		r += (x / i % 10) * i
+		fmt.Println(i, x/i%10, (x/i%10)*i, r)
+	}
+	return x
 }
